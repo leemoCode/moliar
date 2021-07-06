97b9540b86f59be7ba2ba89cf15b13fbe9bc0142
80304198 <lihanshuo@oppo.com>
2021-07-06 17:02:23 +0800

    add home page

--- moliar/src/App.vue
+++ moliar/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Home msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Home,
   },
 })
 export default class App extends Vue {}
---
+++ moliar/src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- 渐进式动画效果 -->
+  <transition name="el-fade-in-linear">
+    <div class="home" v-if="isShowMainPage">
+      <el-button
+        :disabled="true"
+        @click="goMainPage('navigator')"
+        >Navigator
+      </el-button>
+      <el-button
+        :disabled="isLoading"
+        @click="goMainPage('feature_list')"
+        >Feature List
+      </el-button>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import router from '../router';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const isLoading = ref(false);
+    const isShowMainPage = ref(true);
+
+    const goMainPage = (path: string) => {
+      isLoading.value = true;
+
+      setTimeout(() => {
+        isShowMainPage.value = false;
+        router.push({
+          path: '/' + path,
+          // query: {
+          //   id: "666",
+          // },
+        });
+      }, 1000);
+    };
+
+    return {
+      isLoading,
+      isShowMainPage,
+
+      goMainPage,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+}
+</style>
